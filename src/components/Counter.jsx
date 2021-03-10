ccd911ad2c81ee396c382a1580184a3d8ba7f710
José Aroldo <joseharoldoparente@gmail.com>
2021-03-10 07:24:06 -0300

    complete stateComponents

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,17 @@
+import {useState} from 'react';
+
 export function Counter (){
+    
+    const [counter, setCounter] = useState(0);
+    
+    
     function increment () {
-        console.log('Incrementing')
+        setCounter(counter + 1);
     }
 
     return(
         <div>
-            <h2>0</h2>
+            <h2>{counter}</h2>
             <button type="button" onClick={increment}>
                 Increment
             </button>
